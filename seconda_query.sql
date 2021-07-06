9d99d42c10df7bd0dc2405bdeefafada0d252c4e
evelingcastillon <evelingcastillon0304@gmail.com>
2021-07-06 10:15:40 +0200

    complete group by

--- seconda_query.sql
+++ seconda_query.sql
@@ -1,12 +1,27 @@
 -- GROUP BY:
 -- 1. Contare quanti iscritti ci sono stati ogni anno
 
-SELECT COUNT(id)
+SELECT COUNT(id) AS `numeri_studienti` , YEAR(`enrolment_date`) AS `anno_di_iscrizione`
 FROM `students`
-WHERE YEAR(NOW()) - YEAR(`enrolment_date`) = 1
+GROUP BY `anno_di_iscrizione`
+
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT COUNT(id) AS `numero_insegnanti`, `office_address`
+FROM `teachers`
+GROUP BY `office_address`
+
 -- 3. Calcolare la media dei voti di ogni appello d'esame
+
+SELECT AVG(`vote`) AS `media_voti`, `exam_id`
+FROM `exam_student`
+GROUP BY `exam_id`
+
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT COUNT(id) AS `numero_corsi`, `department_id` AS `numero_dipartimento`
+FROM `degrees`
+GROUP BY `department_id`
 
 -- JOINS:
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
